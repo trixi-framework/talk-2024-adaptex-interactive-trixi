d1247fbbf1be380e7dab25d40f3c12e33356f611
Benedict Geihe <bgeihe@uni-koeln.de>
2024-05-10 15:01:46 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,58 @@
+# Interactive Trixi.jl demo
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-success.svg)](https://opensource.org/licenses/MIT)
+
+ADAPTEX Annual meeting, 14th of May, 2024.
+
+To reproduce the live demo of [Trixi.jl](https://github.com/trixi-framework/Trixi.jl)
+shown in the talk, perform the following steps:
+
+### Install Julia
+Go to https://julialang.org/downloads and download the latest stable version of Julia (this
+repository was created with Julia v1.10.3).
+
+### Get reproducibility repository
+Clone this reproducibility repository by executing
+```shell
+git clone https://github.com/trixi-framework/talk-2024-dlr-adaptex-interactive-trixi.git
+```
+
+### Install required packages
+Go to the cloned repository and execute
+```shell
+julia --project=. -e 'import Pkg; Pkg.instantiate()'
+```
+**This will take some time!**
+
+### Start the demo
+
+#### Using Jupyter
+
+Go to the cloned repository and execute
+```shell
+JULIA_NUM_THREADS=4 jupyter-lab demo.ipynb
+```
+(multiple threads are not required, but will be helpful later on)
+
+#### Using Julia directly
+
+To generate the mesh:
+```shell
+julia --project=. scripts/mountain_dome.jl
+```
+
+To run the simulation:
+```shell
+JULIA_NUM_THREADS=4 julia --project=. -e scripts/mountain_gravity_wave.jl
+```
+
+## Authors
+This repository was initiated by
+[Benedict Geihe](https://www.mi.uni-koeln.de/NumSim/dr-benedict-geihe/).
+
+The HOHQMesh Julia scripts were created with great help from 
+[Andrew R. Winters](https://liu.se/en/employee/andwi94)!
+
+
+## License
+The contents of this repository are licensed under the MIT license (see [LICENSE.md](LICENSE.md)).
---
+++ scripts/mountain_mesh.jl
@@ -0,0 +1,53 @@
+using HOHQMesh
+using GLMakie
+
+project = newProject("Mountain", "output_mesh")  # project name, output folder
+
+mountainHeight = 400      # height of montain 400 m
+mountainHalfWidth = 1000  # half width of mountain 1 km
+x_ll = -12000.0           # domain width 40 km
+x_lr =  28000.0
+width = x_lr - x_ll
+
+# lower left and right values according to parametrization
+y_ll = mountainHeight / ( 1 + (x_ll / mountainHalfWidth)^2 )
+y_lr = mountainHeight / ( 1 + (x_lr / mountainHalfWidth)^2 )
+
+# parametrized bottom curve, t ∈ [0,1]
+xEqn = "x(t) = $x_ll + t * $width"
+yEqn = "y(t) = $mountainHeight / ( 1 + (($x_ll + t * $width) / $mountainHalfWidth)^2 )"
+zEqn = "z(t) = 0.0"
+bottom = newParametricEquationCurve("bottom", xEqn, yEqn, zEqn)
+
+# spline curve above
+spline_data = [ [0.0   x_lr                 y_lr          0.0]
+                [0.2   x_ll + 0.9 * width   0.4 * width   0.0]
+                [0.8   x_ll + 0.1 * width   0.4 * width   0.0]
+                [1.0   x_ll                 y_ll          0.0] ]
+
+dome = newSplineCurve("dome", size(spline_data)[1], spline_data)
+
+# use curves as outer boundary
+for curve in [dome, bottom]
+    addCurveToOuterBoundary!(project, curve)
+end
+
+# set the desired resolution in x and y direction
+addBackgroundGrid!(project, [2000.0, 2000.0, 0.0])
+
+# have a look
+plotProject!(project, MODEL+GRID)
+
+# set polynomial degree for curved boundaries
+setPolynomialOrder!(project, 3)
+
+# change output format to ABAQUS
+setMeshFileFormat!(project, "ABAQUS")
+
+# generate
+@info "Press enter to generate the mesh and update the plot."
+readline()
+generate_mesh(project)
+
+@info "Press enter to quit."
+readline()
